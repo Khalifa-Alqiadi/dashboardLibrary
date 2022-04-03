709c1a2362ec813f30c2233460928281cb11064e
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-04-04 02:32:11 +0300

    commit init

--- public/index.php
+++ public/index.php
@@ -28,11 +28,6 @@ $system=new AppSystem($config);
 Router::get('/',[IndexController::class,'list']);
 // Router::get('/',[IndexController::class,'users']);
 Router::get('/categories',[CategoriesController::class,'listAll']);
-Router::get('/add_category',[CategoriesController::class,'create']);
-Router::get('/edit_category/{id}',[CategoriesController::class,'edit']);
-Router::get('/remove_category',[CategoriesController::class,'remove']);
-Router::post('/save_category',[CategoriesController::class,'store']);
-Router::post('/update_category',[CategoriesController::class,'update']);
 
 /** end of web routes */
 
